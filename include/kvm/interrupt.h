68967fcf0dfae9be4d57804d183e27a66a737463
Pekka Enberg <penberg@cs.helsinki.fi>
2010-03-28 19:34:30 +0300

    kvm: Clean up kvm/interrupt.h a bit
    Add include guards to the header file and remove unnecessary #includes.
    
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- include/kvm/interrupt.h
+++ include/kvm/interrupt.h
@@ -1,5 +1,7 @@
+#ifndef KVM__INTERRUPT_H
+#define KVM__INTERRUPT_H
+
 #include <inttypes.h>
-#include <stdbool.h>
 
 #define IVT_BASE	0x0000
 #define IVT_VECTORS	256
@@ -14,3 +16,5 @@ void ivt_copy_table(void *dst, unsigned int size);
 struct ivt_entry * const ivt_get_entry(unsigned int n);
 void ivt_set_entry(struct ivt_entry e, unsigned int n);
 void ivt_set_all(struct ivt_entry e);
+
+#endif /* KVM__INTERRUPT_H */
