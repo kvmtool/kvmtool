6fd7308d51741e1136261346a4b9e72849582b70
Cyrill Gorcunov <gorcunov@gmail.com>
2010-05-30 22:33:27 +0400

    kvm: The command line storage should not be too short
    At moment boot protocol may hold up to 2048 symbols
    from command line we should provide the same compatibility
    for --params option.
    
    Reported-by: Pekka Enberg <penberg@cs.helsinki.fi>
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- main.c
+++ main.c
@@ -30,12 +30,13 @@ static void handle_sigquit(int sig)
 	exit(1);
 }
 
+static char real_cmdline[2048];
+
 int main(int argc, char *argv[])
 {
 	const char *kernel_filename = NULL;
 	const char *kernel_cmdline = NULL;
 	bool single_step = false;
-	char real_cmdline[128];
 	int i;
 
 	signal(SIGQUIT, handle_sigquit);
