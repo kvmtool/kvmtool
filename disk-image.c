fffb37a932e2751dec82b4afbb49c2efed0a7812
Pekka Enberg <penberg@kernel.org>
2011-04-11 21:46:14 +0300

    Revert "kvm tools: Use mmap by default for disk images"
    This reverts commit ca8dc9130dd89ca790a7c0830d1129dfa6c5c69e.
    
    As pointed out by Christoph Helwig, shared writeable mappings are error prone
    and don't handle I/O errors nor ENOSPC properly.

--- disk-image.c
+++ disk-image.c
@@ -13,30 +13,20 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-static int raw_image__read_sector_mmap(struct disk_image *self, uint64_t sector, void *dst, uint32_t dst_len)
+struct disk_image *disk_image__new(int fd, uint64_t size, struct disk_image_operations *ops)
 {
-	uint64_t offset = sector << SECTOR_SHIFT;
+	struct disk_image *self;
 
-	if (offset + dst_len > self->size)
-		return -1;
+	self		= malloc(sizeof *self);
+	if (!self)
+		return NULL;
 
-	memcpy(dst, self->priv + offset, dst_len);
+	self->fd	= fd;
+	self->size	= size;
+	self->ops	= ops;
 
-	return 0;
+	return self;
 }
-
-static int raw_image__write_sector_mmap(struct disk_image *self, uint64_t sector, void *src, uint32_t src_len)
-{
-	uint64_t offset = sector << SECTOR_SHIFT;
-
-	if (offset + src_len > self->size)
-		return -1;
-
-	memcpy(self->priv + offset, src, src_len);
-
-	return 0;
-}
-
 
 static int raw_image__read_sector(struct disk_image *self, uint64_t sector, void *dst, uint32_t dst_len)
 {
@@ -69,27 +59,6 @@ static struct disk_image_operations raw_image_ops = {
 	.write_sector		= raw_image__write_sector,
 };
 
-static struct disk_image_operations raw_image_mmap_ops = {
-	.read_sector		= raw_image__read_sector_mmap,
-	.write_sector		= raw_image__write_sector_mmap,
-};
-
-struct disk_image *disk_image__new(int fd, uint64_t size)
-{
-	struct disk_image *self;
-
-	self		= malloc(sizeof *self);
-	if (!self)
-		return NULL;
-
-	self->fd	= fd;
-	self->size	= size;
-	self->priv	= mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-	self->ops	= (self->priv == MAP_FAILED) ? &raw_image_ops : &raw_image_mmap_ops;
-
-	return self;
-}
-
 static struct disk_image *raw_image__probe(int fd)
 {
 	struct stat st;
@@ -97,7 +66,7 @@ static struct disk_image *raw_image__probe(int fd)
 	if (fstat(fd, &st) < 0)
 		return NULL;
 
-	return disk_image__new(fd, st.st_size);
+	return disk_image__new(fd, st.st_size, &raw_image_ops);
 }
 
 struct disk_image *disk_image__open(const char *filename)
@@ -127,9 +96,6 @@ void disk_image__close(struct disk_image *self)
 
 	if (self->ops->close)
 		self->ops->close(self);
-
-	if (self->priv != MAP_FAILED)
-		munmap(self->priv, self->size);
 
 	if (close(self->fd) < 0)
 		warning("close() failed");
