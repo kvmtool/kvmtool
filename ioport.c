a3598ccd2e0392f73c9b0a3c9d370a7326bfa00e
Pekka Enberg <penberg@kernel.org>
2011-05-21 15:01:05 +0300

    kvm tools: Don't register dummy ops for serial ports
    The serial8250__init() function registers ops for all serial ports before we
    start running the guest kernel so drop the dummy op registrations.
    
    Cc: Cyrill Gorcunov <gorcunov@gmail.com>
    Cc: Sasha Levin <levinsasha928@gmail.com>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- ioport.c
+++ ioport.c
@@ -160,21 +160,9 @@ void ioport__setup_legacy(void)
 	/* 0x00F0 - 0x00FF - Math co-processor */
 	ioport__register(0x00F0, &dummy_write_only_ioport_ops, 2);
 
-	/* PORT 02E8-02EF - serial port, same as 02F8, 03E8 and 03F8 (COM4) */
-	ioport__register(0x02E8, &dummy_read_write_ioport_ops, 7);
-
-	/* PORT 02F8-02FF - serial port, same as 02E8, 03E8 and 03F8 (COM2) */
-	ioport__register(0x02F8, &dummy_read_write_ioport_ops, 7);
-
 	/* PORT 03D4-03D5 - COLOR VIDEO - CRT CONTROL REGISTERS */
 	ioport__register(0x03D4, &dummy_read_write_ioport_ops, 1);
 	ioport__register(0x03D5, &dummy_write_only_ioport_ops, 1);
-
-	/* PORT 03E8-03EF - serial port, same as 02E8, 02F8 and 03F8 (COM3) */
-	ioport__register(0x03E8, &dummy_read_write_ioport_ops, 7);
-
-	/* PORT 03F8-03FF - Serial port (8250,8250A,8251,16450,16550,16550A,etc.) COM1 */
-	ioport__register(0x03F8, &dummy_read_write_ioport_ops, 7);
 
 	/* PORT 0CF8-0CFF - PCI Configuration Mechanism 1 - Configuration Registers */
 	ioport__register(0x0CF8, &dummy_write_only_ioport_ops, 1);
