6c757e718ec8a5398b180563744f5d2bec6ffc84
Sasha Levin <levinsasha928@gmail.com>
2011-07-02 19:52:06 -0400

    kvm tools: Provide instance name when running 'kvm debug'
    Instead of sending a signal to the first instance found, send it
    to a specific instance.
    
    Signed-off-by: Sasha Levin <levinsasha928@gmail.com>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- kvm-debug.c
+++ kvm-debug.c
@@ -1,11 +1,22 @@
-#include <stdio.h>
-#include <string.h>
-
 #include <kvm/util.h>
 #include <kvm/kvm-cmd.h>
 #include <kvm/kvm-debug.h>
+#include <kvm/kvm.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <signal.h>
 
 int kvm_cmd_debug(int argc, const char **argv, const char *prefix)
 {
-	return system("kill -3 $(pidof kvm)");
+	int pid;
+
+	if (argc != 1)
+		die("Usage: kvm debug [instance name]\n");
+
+	pid = kvm__get_pid_by_instance(argv[0]);
+	if (pid < 0)
+		die("Failed locating instance name");
+
+	return kill(pid, SIGQUIT);
 }
