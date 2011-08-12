8d7b77f65f5a595ea40865527b86ab495329ec7b
Sasha Levin <levinsasha928@gmail.com>
2011-08-12 18:20:59 +0300

    kvm tools: Improve 'kvm debug' parameters
    kvm debug now uses the git option parser to parse parameters.
    
    Added option to debug instance by PID and debug all instances.
    
    Improved usage message.
    
    Signed-off-by: Sasha Levin <levinsasha928@gmail.com>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- builtin-debug.c
+++ builtin-debug.c
@@ -8,14 +8,32 @@
 #include <string.h>
 #include <signal.h>
 
+static bool all;
+static u64 instance_pid;
+static const char *instance_name;
+
 static const char * const debug_usage[] = {
-	"kvm debug <instance name>",
+	"kvm debug [--all] [-n name] [-p pid]",
 	NULL
 };
 
 static const struct option debug_options[] = {
+	OPT_GROUP("General options:"),
+	OPT_BOOLEAN('a', "all", &all, "Debug all instances"),
+	OPT_STRING('n', "name", &instance_name, "name", "Instance name"),
+	OPT_U64('p', "pid", &instance_pid, "Instance pid"),
 	OPT_END()
 };
+
+static void parse_debug_options(int argc, const char **argv)
+{
+	while (argc != 0) {
+		argc = parse_options(argc, argv, debug_options, debug_usage,
+				PARSE_OPT_STOP_AT_NON_OPTION);
+		if (argc != 0)
+			kvm_debug_help();
+	}
+}
 
 void kvm_debug_help(void)
 {
@@ -29,18 +47,20 @@ static int do_debug(const char *name, int pid)
 
 int kvm_cmd_debug(int argc, const char **argv, const char *prefix)
 {
-	int pid;
+	parse_debug_options(argc, argv);
 
-	if (argc != 1)
+	if (all)
+		return kvm__enumerate_instances(do_debug);
+
+	if (instance_name == NULL &&
+	    instance_pid == 0)
 		kvm_debug_help();
 
-	if (strcmp(argv[0], "all") == 0) {
-		return kvm__enumerate_instances(do_debug);
-	}
+	if (instance_name)
+		instance_pid = kvm__get_pid_by_instance(argv[0]);
 
-	pid = kvm__get_pid_by_instance(argv[0]);
-	if (pid < 0)
-		die("Failed locating instance name");
+	if (instance_pid <= 0)
+		die("Failed locating instance");
 
-	return kill(pid, SIGQUIT);
+	return kill(instance_pid, SIGQUIT);
 }
