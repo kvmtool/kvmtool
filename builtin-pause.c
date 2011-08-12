477db183e80334c3a50437a35dd48ee290a9b38f
Sasha Levin <levinsasha928@gmail.com>
2011-08-12 18:20:58 +0300

    kvm tools: Improve 'kvm pause' parameters
    kvm pause now uses the git option parser to parse parameters.
    
    Added option to pause instance by PID and pause all instances.
    
    Improved usage message.
    
    Signed-off-by: Sasha Levin <levinsasha928@gmail.com>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- builtin-pause.c
+++ builtin-pause.c
@@ -1,21 +1,39 @@
-#include <stdio.h>
-#include <string.h>
-#include <signal.h>
-
 #include <kvm/util.h>
 #include <kvm/kvm-cmd.h>
 #include <kvm/builtin-pause.h>
 #include <kvm/kvm.h>
 #include <kvm/parse-options.h>
 
+#include <stdio.h>
+#include <string.h>
+#include <signal.h>
+
+static bool all;
+static u64 instance_pid;
+static const char *instance_name;
+
 static const char * const pause_usage[] = {
-	"kvm pause <instance name>",
+	"kvm pause [--all] [-n name] [-p pid]",
 	NULL
 };
 
 static const struct option pause_options[] = {
+	OPT_GROUP("General options:"),
+	OPT_BOOLEAN('a', "all", &all, "Pause all instances"),
+	OPT_STRING('n', "name", &instance_name, "name", "Instance name"),
+	OPT_U64('p', "pid", &instance_pid, "Instance pid"),
 	OPT_END()
 };
+
+static void parse_pause_options(int argc, const char **argv)
+{
+	while (argc != 0) {
+		argc = parse_options(argc, argv, pause_options, pause_usage,
+				PARSE_OPT_STOP_AT_NON_OPTION);
+		if (argc != 0)
+			kvm_pause_help();
+	}
+}
 
 void kvm_pause_help(void)
 {
@@ -29,18 +47,20 @@ static int do_pause(const char *name, int pid)
 
 int kvm_cmd_pause(int argc, const char **argv, const char *prefix)
 {
-	int pid;
+	parse_pause_options(argc, argv);
 
-	if (argc != 1)
+	if (all)
+		return kvm__enumerate_instances(do_pause);
+
+	if (instance_name == NULL &&
+	    instance_pid == 0)
 		kvm_pause_help();
 
-	if (strcmp(argv[0], "all") == 0) {
-		return kvm__enumerate_instances(do_pause);
-	}
+	if (instance_name)
+		instance_pid = kvm__get_pid_by_instance(argv[0]);
 
-	pid = kvm__get_pid_by_instance(argv[0]);
-	if (pid < 0)
-		die("Failed locating instance name");
+	if (instance_pid <= 0)
+		die("Failed locating instance");
 
-	return kill(pid, SIGUSR2);
+	return kill(instance_pid, SIGUSR2);
 }
