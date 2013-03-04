a24ecd1fca4263f0ce79dcbcdafd30032d334b9d
Marc Zyngier <marc.zyngier@arm.com>
2013-03-04 06:27:30 +0000

    kvm tools: init: fix usage of hlist iterators
    Commit b67bfe0d42ca ("hlist: drop the node parameter from iterators")
    incorrectly changed the way that hlist iterators are used.
    
    This patch fixes util/init.c so it passes parameters to the new
    iterators in the right manner.
    
    Cc: Sasha Levin <levinsasha928@gmail.com>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- util/init.c
+++ util/init.c
@@ -33,11 +33,10 @@ int init_list__init(struct kvm *kvm)
 {
 	unsigned int i;
 	int r = 0;
-	struct hlist_node *n;
 	struct init_item *t;
 
 	for (i = 0; i < ARRAY_SIZE(init_lists); i++)
-		hlist_for_each_entry(t, n, &init_lists[i], n) {
+		hlist_for_each_entry(t, &init_lists[i], n) {
 			r = t->init(kvm);
 			if (r < 0) {
 				pr_warning("Failed init: %s\n", t->fn_name);
@@ -53,11 +52,10 @@ int init_list__exit(struct kvm *kvm)
 {
 	int i;
 	int r = 0;
-	struct hlist_node *n;
 	struct init_item *t;
 
 	for (i = ARRAY_SIZE(exit_lists) - 1; i >= 0; i--)
-		hlist_for_each_entry(t, n, &exit_lists[i], n) {
+		hlist_for_each_entry(t, &exit_lists[i], n) {
 			r = t->init(kvm);
 			if (r < 0) {
 				pr_warning("%s failed.\n", t->fn_name);
