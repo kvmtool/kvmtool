f73b9606f5902df73f2f29dfe1b652d04ee39fab
Marc Zyngier <marc.zyngier@arm.com>
2016-03-11 01:52:21 +0000

    kvmtool: arm: Work around missing PMU on AArch32
    We don't have PMU support on 32bit ARM just yet, so let's work
    around this the ugly way for now.
    
    Cc: Will Deacon <will.deacon@arm.com>
    Reported-by: Riku Voipio <riku.voipio@linaro.org>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arm/pmu.c
+++ arm/pmu.c
@@ -6,6 +6,7 @@
 #include "arm-common/gic.h"
 #include "arm-common/pmu.h"
 
+#ifdef CONFIG_ARM64
 static int set_pmu_attr(struct kvm *kvm, int vcpu_idx,
 			struct kvm_device_attr *attr)
 {
@@ -75,3 +76,6 @@ void pmu__generate_fdt_nodes(void *fdt, struct kvm *kvm)
 	_FDT(fdt_property(fdt, "interrupts", irq_prop, sizeof(irq_prop)));
 	_FDT(fdt_end_node(fdt));
 }
+#else
+void pmu__generate_fdt_nodes(void *fdt, struct kvm *kvm) { }
+#endif
