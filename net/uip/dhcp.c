0796825e08da408fba6614d8a135a264d37ef9fe
Andre Przywara <andre.przywara@arm.com>
2019-02-04 16:34:58 +0000

    net/dhcp: avoid misleading strncpy
    The code for copying an empty IP address into the DHCP opt buffer used
    strncpy, however used the source length as the size argument. GCC 8.x
    complains about it.
    
    Since the source string is actually fixed, just revert to the old
    strcpy, which gives us actually the same level of security in this case,
    but makes the compiler happy.
    
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- net/uip/dhcp.c
+++ net/uip/dhcp.c
@@ -131,7 +131,7 @@ static int uip_dhcp_fill_option(struct uip_info *info, struct uip_dhcp *dhcp, in
 	opt[i++]	= UIP_DHCP_TAG_ROOT;
 	opt[i++]	= strlen(EMPTY_ADDR);
 	addr		= (u32 *)&opt[i];
-	strncpy((void *) addr, EMPTY_ADDR, strlen(EMPTY_ADDR));
+	strcpy((void *) addr, EMPTY_ADDR);
 	i		+= strlen(EMPTY_ADDR);
 
 	i 		= uip_dhcp_fill_option_name_and_server(info, opt, i);
