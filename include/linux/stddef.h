d8343698df72d03986efc4b985b2ee18df514200
Anup Patel <apatel@ventanamicro.com>
2023-07-12 22:04:53 +0530

    kvm tools: Add __DECLARE_FLEX_ARRAY() in include/linux/stddef.h
    Latest x86 UAPI headers uses __DECLARE_FLEX_ARRAY() macro so let us take
    this macro from Linux UAPI header and add it to include/linux/stddef.h.
    
    Signed-off-by: Anup Patel <apatel@ventanamicro.com>
    Link: https://lore.kernel.org/r/20230712163501.1769737-2-apatel@ventanamicro.com
    Signed-off-by: Will Deacon <will@kernel.org>

--- include/linux/stddef.h
+++ include/linux/stddef.h
@@ -7,4 +7,20 @@
 #undef offsetof
 #define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
 
+/**
+ * __DECLARE_FLEX_ARRAY() - Declare a flexible array usable in a union
+ *
+ * @TYPE: The type of each flexible array element
+ * @NAME: The name of the flexible array member
+ *
+ * In order to have a flexible array member in a union or alone in a
+ * struct, it needs to be wrapped in an anonymous struct with at least 1
+ * named member, but that member can be empty.
+ */
+#define __DECLARE_FLEX_ARRAY(TYPE, NAME)	\
+	struct { \
+		struct { } __empty_ ## NAME; \
+		TYPE NAME[]; \
+	}
+
 #endif
